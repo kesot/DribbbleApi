67aa7e3bf0ba31de23e9ecd2e185e6d3cfd54853
kesot <kesot@mail.ru>
2014-10-28 20:35:22 +0300

    2
    token getted

---
+++ DribbleApi/DribbleApi/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace DribbleApi
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// webBrowser1
+			// 
+			this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.webBrowser1.Location = new System.Drawing.Point(3, 18);
+			this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowser1.Name = "webBrowser1";
+			this.webBrowser1.Size = new System.Drawing.Size(863, 441);
+			this.webBrowser1.TabIndex = 0;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.webBrowser1);
+			this.groupBox1.Location = new System.Drawing.Point(3, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(869, 462);
+			this.groupBox1.TabIndex = 1;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "groupBox1";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(898, 55);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(223, 22);
+			this.textBox1.TabIndex = 2;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(941, 108);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1178, 486);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.groupBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.WebBrowser webBrowser1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ DribbleApi/DribbleApi/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace DribbleApi
+{
+	public partial class Form1 : Form
+	{
+		private static readonly string TokenUri = "https://dribbble.com/oauth/token";
+		private static readonly string AuthorizationUri = "https://dribbble.com/oauth/authorize";
+		private static readonly string ClientIdString = "ee9ff82e0021b503d34d0a3fe210f4d56eb16e0b1df438ba501bac33643b74c4";
+		private static readonly string ClientSecretString = "7c213243b7671fca8a99ae62b701fbfee4256feb8f56bca85cfca305f2162d9b";
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			//webBrowser1.Navigate("http://localhost:123/?code=06cef3a5d7699f4989317f10000c192c69a9c20372d305c3ca67fde3a85d0473");
+			webBrowser1.Navigated += OnNavigate;
+			TcpListener tl = new TcpListener(8080);
+			tl.Start();
+			ThreadPool.QueueUserWorkItem(o =>
+			{
+				while (true)
+				{
+					var tc = tl.AcceptTcpClient();
+					ThreadPool.QueueUserWorkItem(u =>
+					{
+						string Html = "<html><body><h1>It works!</h1></body></html>";
+						// Необходимые заголовки: ответ сервера, тип и длина содержимого. После двух пустых строк - само содержимое
+						string Str = "HTTP/1.1 200 OK\nContent-type: text/html\nContent-Length:" + Html.Length.ToString() + "\r\n\r\n" + Html;
+						// Приведем строку к виду массива байт
+						byte[] Buffer = Encoding.ASCII.GetBytes(Str);
+						// Отправим его клиенту
+						//while ((tc.GetStream().ReadByte()) != -1) ;
+						tc.GetStream().Write(Buffer, 0, Buffer.Length);
+						// Закроем соединение
+						tc.Close();
+					});
+				}
+			});
+			webBrowser1.Navigate("https://dribbble.com/oauth/authorize?client_id="+ClientIdString);
+			
+		}
+
+		private string code;
+		void Authorize()
+		{
+			var uri =
+				TokenUri +
+				"?client_id=" + ClientIdString +
+				"&client_secret=" + ClientSecretString +
+				"&code=" + code;
+			HttpWebRequest request =
+				WebRequest.CreateHttp(uri);
+
+			//request.Headers.Add("Authorization", "DirectCrm key=\"zO3d06YKthe9R9h7UIFb\" customerId=\"\" sessionKey=\"\"");
+			request.Method = "POST";
+			WebResponse resp = request.GetResponse();
+			
+		}
+		void OnNavigate(object sender, EventArgs e)
+		{
+			if (webBrowser1.Url.Host == "localhost" && !webBrowser1.Url.Query.Contains("error"))
+			{
+				code = webBrowser1.Url.Query.Replace("?code=", "");
+				Authorize();
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = webBrowser1.Url.ToString();
+		}
+		
+	}
+}
